02f394486e4f9c95fb8dce5344767a5c849917a1
Kouka Bassila <bassilakouka@gmail.com>
2026-01-13 09:50:57 +0100

    views.py

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -2,9 +2,23 @@ from app import app
 from flask import render_template, request, jsonify
 
 ### EXO1 - simple API
+@app.route('/api/salutation', methods=['GET'])
+def salutation():
+    return jsonify({"message": "Salut"})
 
 ### EXO2 - API with simple display
+@app.route('/simple-html')
+def simple_display():
+    return "<h1>HTML simple </h1><p>Par El_Flaco27</p>"
 
 ### EXO3 - API with parameters display 
+@app.route('/')
+def index():
+    user={'name':'javier','surname':'pastore'}
+    return render_template('index.html',title='MDM',utilisateur=user)
 
-### EXO4 - API with parameters retrieved from URL 
+### EXO4 - API with parameters retrieved from URL
+@app.route('/user/<name>/<surname>')
+def user(name,surname):
+    user={'name':name,'surname':surname}
+    return render_template('index.html',title='MDM',utilisateur=user)
